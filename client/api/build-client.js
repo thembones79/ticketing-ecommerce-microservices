e51860b545297a40cf3e4b7216a3af76863799f1
thembones79 <michal.szumnarski@gmail.com>
2020-12-04 15:10:46 +0100

    fixed ingtess and changed https to http

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -8,8 +8,7 @@ const buildClient = ({ req }) => {
     // http://SERVICENAME.NAMESPACE.svc.cluster.local
 
     return axios.create({
-      baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      baseURL: "http://www.ticketing-app-prod.xyz/",
       headers: req.headers,
     });
   } else {
